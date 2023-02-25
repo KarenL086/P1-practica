ecf8e1c7a285c2a5f4fe6c738f76b16a3e013c55
Karen <karenlm086@gmail.com>
2023-02-25 16:41:05 -0600

    Agregar archivos de proyecto.

---
+++ P1-practica/Controllers/equiposController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using P1_practica.Models;
+
+namespace P1_practica.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class equiposController : ControllerBase
+    {
+        private readonly equiposContext _equiposContexto;
+
+        public equiposController(equiposContext equiposContexto)
+        {
+            _equiposContexto = equiposContexto;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult Get()
+        {
+            List<equipos> listadoEquipos = (from e in _equiposContexto.equipos
+                                            select e).ToList();
+
+            if(listadoEquipos.Count == 0)   
+            {
+                return NotFound();
+            }
+
+            return Ok(listadoEquipos);
+        }
+    }
+}
